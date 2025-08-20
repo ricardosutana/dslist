d04a37ed42a5adab7f913a6af2adebf12f4296a6
ricardosutana <ricardosutana@gmail.com>
2025-08-19 23:15:50 -0300

    create the controller layer and services layer

---
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repositories.GameRepository;
+
+@Service // indicates that this class is a service component in the Spring framework
+public class GameService {
+    // Business logic related to "Game" entities will be implemented here
+
+    @Autowired
+    private GameRepository gameRepository;
+    
+    public List<Game> findAll() {
+        // This method will contain logic to retrieve all games and convert them to GameMinDTOs
+    List<Game> result = gameRepository.findAll();
+    return result; 
+
+        }
+    }
